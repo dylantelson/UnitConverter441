c2647d538db7a794f9baefd5722a7a330a683006
Dylan Telson <dtelson1@binghamton.edu>
2019-08-29 18:41:27 -0400

    Added miles, centimeters, and kilometers

--- app/src/main/java/com/example/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         Spinner firstDropdown = findViewById(R.id.dropdown1);
         Spinner secondDropdown = findViewById(R.id.dropdown2);
-        String[] items = new String[]{"feet", "inches", "yards", "meters"};
+        String[] items = new String[]{"feet", "inches", "yards", "meters", "centimeters", "kilometers", "miles"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         firstDropdown.setAdapter(adapter);
         secondDropdown.setAdapter(adapter);
@@ -38,7 +38,10 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener((view) -> {
             if(firstDropdown.getSelectedItem().toString() == secondDropdown.getSelectedItem().toString())
+            {
                 resulttv.setText(inputtv.getText().toString());
+                return;
+            }
             else resulttv.setText(convert(firstDropdown.getSelectedItem().toString(), secondDropdown.getSelectedItem().toString(), inputtv.getText().toString()));
             int index = resulttv.getText().toString().indexOf(".");
             if(Float.parseFloat(resulttv.getText().toString()) == Math.ceil(Float.parseFloat(resulttv.getText().toString()))) {
@@ -54,22 +57,62 @@ public class MainActivity extends AppCompatActivity {
     public String convert(String inputType, String outputType, String input) {
         if(inputType == "feet") {
             if(outputType == "inches") return "" + (Float.parseFloat(input) * 12);
-            if(outputType == "yards") return "" + (Float.parseFloat(input) / 3);
-            if(outputType == "meters") return "" + (Float.parseFloat(input) / 3.281);
+            else if(outputType == "yards") return "" + (Float.parseFloat(input) / 3);
+            else if(outputType == "meters") return "" + (Float.parseFloat(input) / 3.281);
+            else if(outputType == "centimeters") return "" + (Float.parseFloat(input) * 30.48);
+            else if(outputType == "kilometers") return "" + (Float.parseFloat(input) / 3280.84);
+            else if(outputType == "miles") return "" + (Float.parseFloat(input) / 5280);
         }
-        if(inputType == "inches") {
+        else if(inputType == "inches") {
             if(outputType == "feet") return "" + (Float.parseFloat(input) / 12);
-            if(outputType == "yards") return "" + (Float.parseFloat(input) / 36);
-            if(outputType == "meters") return "" + (Float.parseFloat(input) / 39.37);
+            else if(outputType == "yards") return "" + (Float.parseFloat(input) / 36);
+            else if(outputType == "meters") return "" + (Float.parseFloat(input) / 39.37);
+            else if(outputType == "centimeters") return "" + (Float.parseFloat(input) * 2.54);
+            else if(outputType == "kilometers") return "" + (Float.parseFloat(input) / 39370.079);
+            else if(outputType == "miles") return "" + (Float.parseFloat(input) / 63360);
         }
-        if(inputType == "yards") {
+        else if(inputType == "yards") {
             if(outputType == "feet") return "" + (Float.parseFloat(input) * 3);
-            if(outputType == "inches") return "" + (Float.parseFloat(input) * 36);
-            if(outputType == "meters") return "" + (Float.parseFloat(input) / 1.094);
+            else if(outputType == "inches") return "" + (Float.parseFloat(input) * 36);
+            else if(outputType == "meters") return "" + (Float.parseFloat(input) / 1.094);
+            else if(outputType == "centimeters") return "" + (Float.parseFloat(input) * 91.44);
+            else if(outputType == "kilometers") return "" + (Float.parseFloat(input) / 1093.613);
+            else if(outputType == "miles") return "" + (Float.parseFloat(input) / 1760);
         }
-        if(outputType == "feet") return "" + (Float.parseFloat(input) * 3.281);
-        if(outputType == "inches") return "" + (Float.parseFloat(input) * 39.37);
-        return "" + (Float.parseFloat(input) * 1.094);
+        else if(inputType == "meters") {
+            if(outputType == "feet") return "" + (Float.parseFloat(input) * 3.281);
+            else if(outputType == "inches") return "" + (Float.parseFloat(input) * 39.37);
+            else if(outputType == "yards") return "" + (Float.parseFloat(input) * 1.094);
+            else if(outputType == "centimeters") return "" + (Float.parseFloat(input) * 100);
+            else if(outputType == "kilometers") return "" + (Float.parseFloat(input) / 1000);
+            else if(outputType == "miles") return "" + (Float.parseFloat(input) / 1609.344);
+        }
+        else if(inputType == "centimeters") {
+            if(outputType == "feet") return "" + (Float.parseFloat(input) * 3.281);
+            else if(outputType == "inches") return "" + (Float.parseFloat(input) * 39.37);
+            else if(outputType == "yards") return "" + (Float.parseFloat(input) / 91.44);
+            else if(outputType == "meters") return "" + (Float.parseFloat(input) / 100);
+            else if(outputType == "kilometers") return "" + (Float.parseFloat(input) / 100000);
+            else if(outputType == "miles") return "" + (Float.parseFloat(input) / 160934.4);
+        }
+        else if(inputType == "kilometers") {
+            if(outputType == "feet") return "" + (Float.parseFloat(input) * 3280.84);
+            else if(outputType == "inches") return "" + (Float.parseFloat(input) * 39370.079);
+            else if(outputType == "yards") return "" + (Float.parseFloat(input) * 1093.613);
+            else if(outputType == "centimeters") return "" + (Float.parseFloat(input) * 100000);
+            else if(outputType == "meters") return "" + (Float.parseFloat(input) * 1000);
+            else if(outputType == "miles") return "" + (Float.parseFloat(input) / 1.609);
+        }
+        //Miles conversion
+        else {
+            if(outputType == "feet") return "" + (Float.parseFloat(input) * 5280);
+            if(outputType == "inches") return "" + (Float.parseFloat(input) * 63360);
+            if(outputType == "yards") return "" + (Float.parseFloat(input) * 1760);
+            if(outputType == "meters") return "" + (Float.parseFloat(input) * 1609.344);
+            if(outputType == "centimeters") return "" + (Float.parseFloat(input) * 160934.4);
+            return "" + (Float.parseFloat(input) * 1.609);
+        }
+        return "0";
     }
 
     @Override
